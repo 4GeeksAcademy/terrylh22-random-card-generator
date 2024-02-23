7145ba7442d7ee5256dffa87ef0923589ed5e5b6
terry howell <63936327+terrylh22@users.noreply.github.com>
2024-02-23 23:24:14 +0000

    fixed styles.css link

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 window.onload = function() {
-
   //generate randomness for suit and number
-  const suits = ['♦', '♥', '♠', '♣'];
+  const suits = ["♦", "♥", "♠", "♣"];
   const randomSuitIndex = Math.floor(Math.random() * suits.length);
   const randomSuit = suits[randomSuitIndex];
   console.log(randomSuit);
 
-  const values = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+  const values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
   const randomValueIndex = Math.floor(Math.random() * values.length);
   const randomValue = values[randomValueIndex];
   console.log(randomValue);
@@ -18,13 +17,13 @@ window.onload = function() {
   const topTextNode = document.createTextNode(randomSuit);
   topRowElement.appendChild(topTextNode);
   topRow.appendChild(topRowElement);
-  
+
   const middleRow = document.querySelector(".middle-row");
   const middleRowElement = document.createElement("p");
   const middleTextNode = document.createTextNode(randomValue);
   middleRowElement.appendChild(middleTextNode);
   middleRow.appendChild(middleRowElement);
-  
+
   const bottomRow = document.querySelector(".bottom-row");
   const bottomRowElement = document.createElement("p");
   bottomRowElement.classList.add("bottom-icon");
@@ -32,10 +31,9 @@ window.onload = function() {
   bottomRowElement.appendChild(bottomTextNode);
   bottomRow.appendChild(bottomRowElement);
 
-  if (randomSuit === '♦' || randomSuit === '♥') {
+  if (randomSuit === "♦" || randomSuit === "♥") {
     bottomRowElement.classList.add("red-text");
     topRowElement.classList.add("red-text");
     middleRowElement.classList.add("red-text");
-    
   }
-}
+};
